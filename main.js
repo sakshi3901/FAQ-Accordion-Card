3e852e5343ebe9527ee1a4f5b9dccecc352d247f
sakshi3901 <53908497+sakshi3901@users.noreply.github.com>
2023-02-11 21:03:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let questions = document.querySelectorAll(".questions");
+let answer = document.querySelectorAll(".answers");
+
+questions.forEach((question) =>
+  question.addEventListener('click', () => {
+
+      if (question.parentNode.classList.contains('active')) {
+        question.parentNode.classList.toggle('active');
+      } else {
+        questions.forEach((question) => question.parentNode.classList.remove('active'))
+        question.parentNode.classList.add('active');
+      }
+  })
+);
